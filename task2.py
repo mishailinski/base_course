f7ffe5cdb0eff577cd685859434f6e4c035e4b55
mishailinski <eevend203@gmail.com>
2024-12-08 17:39:26 +0000

    less6

--- task2.py
+++ task2.py
@@ -1,18 +1,48 @@
 import matplotlib.pyplot as plt
 import numpy as np
-nul = 0
 def hyper(x_min,x_max,N):
-    x1 = np.linspace(-10, -0.01, 100)
-    x2 = np.linspace(0.01, 10, 100)
-    y = 1/x1
-    plt.plot(x1,y)
-    y = 1/x2
-    plt.plot(x2,y)
+    if x_min < x_max:
+        if (x_min < 0) and (x_max > 0):
+            polN = int(N/2)
+            x1 = np.linspace(x_min, -0.01, polN)
+            x2 = np.linspace(0.01, x_max, polN)
+            y = 1/x1
+            plt.plot(x1,y, color= 'k')
+            y = 1/x2
+            plt.plot(x2,y, color = 'k')
+        elif (x_min >= 0) and (x_max != 0):
+            if x_min != 0:
+                x = np.linspace(x_min, x_max, N)
+                y = 1/x
+                plt.plot(x,y, color= 'k')
+            else:
+                x = np.linspace(0.01, x_max, N)
+                y = 1/x
+                plt.plot(x,y, color= 'k')
+        elif (x_max <= 0) and (x_min != 0):
+            if x_max != 0:
+                x = np.linspace(x_min, x_max, N)
+                y = 1/x
+                plt.plot(x,y, color= 'k')
+            else:
+                x = np.linspace(x_min, -0.01, N)
+                y = 1/x
+                plt.plot(x,y, color= 'k')
+        elif (x_max == 0) and (x_min == 0):
+            print('Недопустимое значение')
+        	
+        plt.title('Гипербола')
+    else:
+        print('Недопустимое значение')
 
-x_min = int(-10)
-x_max = int(10)
-N = int(100)
-polN = N/2
+
+
+
+x_min = int(input('Введите X минимальное '))
+x_max = int(input('Введите X максимальное '))
+N = int(input('Введите количество точек '))
+
+
 
 hyper(x_min,x_max,N)
 plt.savefig('fig_2.png')
